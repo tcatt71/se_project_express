2632be61f1356b6e5a028722a861d91c49b7afd4
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2026-05-07 17:56:43 -0700

    Create Schemas and Models

---
+++ models/clothingItem.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const clothingItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 30,
+  },
+  weather: { type: String, required: true, enum: ["hot", "warm", "cold"] },
+  imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+  owner: { type: mongoose.Schema.Types.ObjectId, required: true },
+  likes: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
+    default: [],
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("clothingItem", clothingItemSchema);
