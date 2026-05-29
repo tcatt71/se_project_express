80e6403e28b133b83c44c0f56d2b367e6a38fdfb
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2026-05-29 04:35:44 -0700

    fix(server): handle database connection failures

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const mainRouter = require("./routes/index");
 const { PORT = 3001 } = process.env;
 const app = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .catch(() => process.exit(1));
 
 app.use(cors());
 app.use(express.json());
