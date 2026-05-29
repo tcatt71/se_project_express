8ff0e081ea53da5e992a04079dea3ef4665943c8
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2026-05-29 03:50:09 -0700

    chore(deps): install cors package

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const mainRouter = require("./routes/index");
 
 const { PORT = 3001 } = process.env;
@@ -7,6 +9,7 @@ const app = express();
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
+app.use(cors());
 app.use(express.json());
 app.use("/", mainRouter);
 
