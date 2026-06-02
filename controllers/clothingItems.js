4b78464dee2323e9371ebd1f9617f47d41624007
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2026-06-02 00:38:03 -0700

    refactor(items-controllers): convert deleteItem and toggleLike to async/await

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -28,38 +28,41 @@ async function createItem(req, res) {
   }
 }
 
-function deleteItem(req, res) {
+async function deleteItem(req, res) {
   const { itemId } = req.params;
 
-  ClothingItem.findById(itemId)
-    .orFail()
-    .then((clothingItem) => {
-      if (clothingItem.owner.toString() !== req.user._id) {
-        const err = new Error();
-        err.name = "ForbiddenError";
+  try {
+    const clothingItem = await ClothingItem.findById(itemId).orFail();
 
-        throw err;
-      }
+    if (clothingItem.owner.toString() !== req.user._id) {
+      const err = new Error();
+      err.name = "ForbiddenError";
+      throw err;
+    }
 
-      return clothingItem.deleteOne();
-    })
-    .then((clothingItem) => sendSuccessResponse(res, clothingItem))
-    .catch((err) => sendErrorResponse(res, err));
+    await clothingItem.deleteOne();
+    return sendSuccessResponse(res, clothingItem);
+  } catch (err) {
+    return sendErrorResponse(res, err);
+  }
 }
 
-function toggleLike(req, res) {
+async function toggleLike(req, res) {
   const { itemId } = req.params;
   const { _id: userId } = req.user;
   const operation = req.method === "DELETE" ? "$pull" : "$addToSet";
 
-  ClothingItem.findByIdAndUpdate(
-    itemId,
-    { [operation]: { likes: userId } },
-    { new: true }
-  )
-    .orFail()
-    .then((like) => sendSuccessResponse(res, like))
-    .catch((err) => sendErrorResponse(res, err));
+  try {
+    const like = await ClothingItem.findByIdAndUpdate(
+      itemId,
+      { [operation]: { likes: userId } },
+      { new: true }
+    ).orFail();
+
+    return sendSuccessResponse(res, like);
+  } catch (err) {
+    return sendErrorResponse(res, err);
+  }
 }
 
 module.exports = { getItems, createItem, deleteItem, toggleLike };
