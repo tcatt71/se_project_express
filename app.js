aa509697da885e0a2b86f9e625739021399540fa
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2026-08-06 04:58:56 -0700

    feat: create centralized error handler and use in app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const mainRouter = require("./routes/index");
+const errorHandler = require("./middlewares/errorHandler");
 
 const { PORT = 3001 } = process.env;
 const app = express();
@@ -14,5 +15,6 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use("/", mainRouter);
+app.use(errorHandler);
 
 app.listen(PORT);
---
+++ middlewares/errorHandler.js
@@ -0,0 +1,11 @@
+function errorHandler(err, req, res, next) {
+  console.log(err);
+
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode).send({
+    message: statusCode === 500 ? "An error occurred on the server" : message,
+  });
+}
+
+module.exports = errorHandler;
