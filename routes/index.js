8a0ab8a0748e03ea2278b42aac6204dcc2f377c6
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2026-08-12 02:48:08 -0700

    refactor: pass unknown route 404s to central errorHandler

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 const usersRouter = require("./users");
 const clothingItemsRouter = require("./clothingItems");
 const { createUser, login } = require("../controllers/users");
-const { NOT_FOUND } = require("../utils/errors");
+
+const { NotFoundError } = require("../middlewares/errorHandler");
 
 router.post("/signin", login);
 router.post("/signup", createUser);
@@ -10,10 +11,8 @@ router.post("/signup", createUser);
 router.use("/", clothingItemsRouter);
 router.use("/", usersRouter);
 
-router.use((req, res) =>
-  res.status(NOT_FOUND).json({
-    message: "Requested resource not found",
-  })
+router.use((req, res, next) =>
+  next(new NotFoundError("Requested resource not found"))
 );
 
 module.exports = router;
