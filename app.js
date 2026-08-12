7a4f4eb6152e286519358583db66c3e5ca1a16f9
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2026-08-12 03:01:44 -0700

    feat: mount celebrate errors middleware in app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const { errors } = require("celebrate");
+
+const mainRouter = require("./routes/index");
+const errorHandler = require("./middlewares/errorHandler");
+
+const { PORT = 3001 } = process.env;
+const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .catch(() => process.exit(1));
+
+app.use(cors());
+app.use(express.json());
+app.use("/", mainRouter);
+app.use(errors());
+app.use(errorHandler);
+
+app.listen(PORT);
