daa5ceb3b6a582f7e305114f228c07a6de106fcd
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2026-08-19 21:03:02 -0700

    chore: add server crash test endpoint

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
 app.use("/", mainRouter);
 app.use(errorLogger);
 app.use(errors());
