0c86b2be3421f3748c6ee30c36667ebfc3c68582
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2026-08-23 00:17:49 -0700

    chore: remove unused createAuthError function

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,13 +2,6 @@ function sendSuccessResponse(res, data, statusCode = 200) {
   return res.status(statusCode).json(data);
 }
 
-function createAuthError() {
-  const error = new Error();
-  error.name = "AuthenticationError";
-  return error;
-}
-
 module.exports = {
   sendSuccessResponse,
-  createAuthError,
 };
