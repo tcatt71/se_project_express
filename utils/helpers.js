33f25c9531a9ab0c7f410ea2b468c35bcac99e41
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2026-08-23 00:13:27 -0700

    chore: remove unused sendErrorResponse function and references

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,49 +1,3 @@
-const {
-  BAD_REQUEST,
-  UNAUTHORIZED,
-  FORBIDDEN,
-  NOT_FOUND,
-  CONFLICT,
-  INTERNAL_SERVER_ERROR,
-} = require("./errors");
-
-function sendErrorResponse(res, err) {
-  if (err.name === "CastError") {
-    return res.status(BAD_REQUEST).json({ message: "Invalid input provided" });
-  }
-  if (err.name === "ValidationError") {
-    return res
-      .status(BAD_REQUEST)
-      .json({ message: "Missing or invalid data provided" });
-  }
-  if (err.name === "AuthenticationError") {
-    return res
-      .status(UNAUTHORIZED)
-      .json({ message: "Incorrect email or password" });
-  }
-  if (err.name === "UnauthorizedError") {
-    return res.status(UNAUTHORIZED).json({ message: "Authorization required" });
-  }
-  if (err.name === "ForbiddenError") {
-    return res
-      .status(FORBIDDEN)
-      .json({ message: "You are not allowed to perform this operation" });
-  }
-  if (err.name === "DocumentNotFoundError") {
-    return res
-      .status(NOT_FOUND)
-      .json({ message: "Requested resource not found" });
-  }
-  if (err.code === 11000) {
-    return res
-      .status(CONFLICT)
-      .json({ message: "An account with this email already exists." });
-  }
-  return res
-    .status(INTERNAL_SERVER_ERROR)
-    .json({ message: "An error has occurred on the server." });
-}
-
 function sendSuccessResponse(res, data, statusCode = 200) {
   return res.status(statusCode).json(data);
 }
@@ -55,7 +9,6 @@ function createAuthError() {
 }
 
 module.exports = {
-  sendErrorResponse,
   sendSuccessResponse,
   createAuthError,
 };
